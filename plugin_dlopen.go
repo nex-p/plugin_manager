e719ee8ff7a2139a9f5b804ef5ca9cb54ad82d84
Kalana Dhananjaya <kalanad@eng.pdn.ac.lk>
2023-04-18 03:46:55 +0000

    update link function name

--- plugin_dlopen.go
+++ plugin_dlopen.go
@@ -141,7 +141,7 @@ var (
 )
 
 // lastmoduleinit is defined in package runtime.
-//go:linkname lastmoduleinit runtime.lastmoduleinit
+//go:linkname lastmoduleinit plugin_manager.lastmoduleinit
 func lastmoduleinit() (pluginpath string, syms map[string]any, errstr string)
 
 // doInit is defined in package runtime.
